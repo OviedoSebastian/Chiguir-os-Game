646b7e926587e01b9a0da796927f46e4e206fa40
SebastianOviedo <juan.sebastian.oviedo@correounivalle.edu.co>
2024-03-05 17:24:57 -0500

    Add files via upload

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,12 +1,13 @@
-import { OrbitControls } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
 import { useRef } from "react";
 import { Plane } from "@react-three/drei";
 
 const Experience = () => {
 
     const planeRef = useRef(null);
-    const cameraRef = useRef(null);
+    const boxRef = useRef(null);
+    // const { camera } = useThree
 
     useFrame((state, delta)=>{
         // planeRef.current.rotation.x += 1 * delta;
@@ -17,13 +18,18 @@ const Experience = () => {
             <ambientLight />
             <directionalLight position={[10, 10, 5]} />
             
-            <mesh ref={planeRef}> 
-                <Plane args={[10, 100]} />
-                <meshStandardMaterial color={"purple"} />
+            <mesh ref={planeRef} position={[0, 0, 0]}> 
+                <Plane args={[10, 100]} rotation={[0,0,0]}/>
+                <meshStandardMaterial color={'green'} />
             </mesh>
-            
+
+            {/* <mesh position={[0, 5, 0]} ref={boxRef}>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshDistanceMaterial />
+            </mesh> */}
             {/* Control de movimiento de la camara */}
-            <OrbitControls enableZoom enablePan enableRotate />
+            <OrbitControls />
+            <PerspectiveCamera position={[0, 0, 50]} makeDefault />
         </>
 
     )
