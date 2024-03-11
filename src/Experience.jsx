ed68d8657b444615d7f559fef955a7d693dfae01
SebastianOviedo <juan.sebastian.oviedo@correounivalle.edu.co>
2024-03-11 16:36:17 -0500

    Implementacion del resto de materiales

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,50 +1,88 @@
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
-import { useFrame, useThree } from "@react-three/fiber";
+import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import { Plane } from "@react-three/drei";
 
 const Experience = () => {
-
-    const planeRef = useRef(null);
     const boxRef = useRef(null);
-    // const { camera } = useThree
+    // const boxdistanceRef = useRef(null);
+    const boxnormaleRef = useRef(null);
+    const boxlambertRef = useRef(null);
+    const boxmatcapRef = useRef(null);
+    const boxphongRef = useRef(null);
+    const boxphysicalgRef = useRef(null);
+    const boxtoongRef = useRef(null);
+    const planeRef = useRef(null);
+    
+    useFrame(({clock}, delta) => {
 
-    useFrame((state, delta)=>{
-        // planeRef.current.rotation.x += 1 * delta;
-    })
+        boxRef.current.rotation.y = Math.cos(clock.getElapsedTime());
+        boxnormaleRef.current.rotation.y = Math.sin(clock.getElapsedTime());
+        boxlambertRef.current.rotation.y = Math.cos(clock.getElapsedTime());
+        boxmatcapRef.current.rotation.y = Math.sin(clock.getElapsedTime());
+        boxphongRef.current.rotation.y = Math.cos(clock.getElapsedTime());
+        boxphysicalgRef.current.rotation.y = Math.sin(clock.getElapsedTime());
+        boxtoongRef.current.rotation.y = Math.cos(clock.getElapsedTime());
+    });
+
 
     return (
         <>
             <ambientLight />
             <directionalLight position={[10, 10, 5]} />
-            
-            <mesh ref={planeRef} position={[0, 0, 0]}> 
-                <Plane args={[10, 100]} rotation={[0,0,0]}/>
-                <meshStandardMaterial color={'green'} />
+
+            <mesh position={[0, 0, 1]} ref={boxRef}>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshStandardMaterial color={"purple"} />
             </mesh>
 
-            {/* <mesh position={[0, 5, 0]} ref={boxRef}>
+            {/* Al agregar el distance material el proyecto muere */}
+            {/* <mesh position={[4, 0, 1]} ref={boxdistanceRef}>
                 <boxGeometry args={[1, 1, 1]} />
-                <meshDistanceMaterial />
+                <meshDistanceMaterial color = {"white"}/>
             </mesh> */}
-            {/* Control de movimiento de la camara */}
+        
+            <mesh position={[4, 0, 1]} ref={boxnormaleRef}>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshNormalMaterial  color = {"white"}/>
+            </mesh>
+
+            <mesh position={[2, 0, 1]} ref={boxlambertRef}>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshLambertMaterial  color = {"white"}/>
+            </mesh>
+
+            <mesh position={[-2, 0, 1]} ref={boxmatcapRef}>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshMatcapMaterial color = {"blue"}/>
+            </mesh>
+
+            <mesh position={[-4, 0, 1]} ref={boxphongRef}>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshPhongMaterial color = {"red"}/>
+            </mesh>
+
+            <mesh position={[-6, 0, 1]} ref={boxphysicalgRef}>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshPhongMaterial color = {"orange"}/>
+            </mesh>
+
+            <mesh position={[6, 0, 1]} ref={boxtoongRef}>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshPhongMaterial color = {"grey"}/>
+            </mesh>
+
+            <mesh ref={planeRef}  position={[0, 0, 0]} rotation={[0, 0, 0]} scale={[10, 1, 1]}> 
+                <Plane  args={[100, 10]}/>
+                <meshBasicMaterial  color={'grey'} />
+            </mesh>
+
+            {/* <PerspectiveCamera enableZoom enablePan enableRotate position={[1, 5, 7]} /> */}
             <OrbitControls />
-            <PerspectiveCamera position={[0, 0, 50]} makeDefault />
+        
         </>
 
     )
 }
 
 export default Experience;
-
-// const Experience = ({ title, subtitle }) => {
-
-//     return (
-//         <>
-//             <h1 className="title"> {title} </h1>
-//             <span> {subtitle} </span>
-//         </>
-//     )
-// }
-
-// export default Experience;
