1143567cb2a7522310b888615eb8b003a30980b9
SebastianOviedo <juan.sebastian.oviedo@correounivalle.edu.co>
2024-04-24 14:07:12 -0500

    addLogin

--- src/routes/RoutesSquidGames.jsx
+++ src/routes/RoutesSquidGames.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Level1 from "../pages/level1/Level1";
 import Login from "../pages/login/Login";
+import SelectLevel from "../pages/login/SelectLevel";
 
 export default function RoutesSquidGames() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Login />} />
+                <Route path="/chooselevel" element={<SelectLevel />} />
                 <Route path="/level1" element={<Level1 />} />
             </Routes>
         </BrowserRouter>
