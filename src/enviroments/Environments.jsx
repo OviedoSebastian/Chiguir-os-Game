ef7abfaa55a98fd82d510524825e0fbf826a9e9c
Camilo <ivan.morales@correounivalle.edu.co>
2024-04-25 10:21:28 -0500

    adding sounds and hdri

--- src/enviroments/Environments.jsx
+++ src/enviroments/Environments.jsx
@@ -1,29 +1,12 @@
-import { Cloud, Environment, Sky, Sparkles, Stars } from "@react-three/drei";
 
 export default function Environments() {
   return (
     <>
       {/* <Environment
-            files={"/assets/hdris/escene.hdr"}
+            files={"/assets/hdris/sky.hdr"}
             preset= {null}
             background= {true}
         /> */}
-      <Sky />
-      <Stars />
-      <Sparkles 
-        count={100}
-        size={3}
-        color={"yellow"}
-        scale={2}
-      />
-      <Cloud 
-        bounds={[10, 2, 100]} 
-        volume={10} 
-        color={"aqua"}
-        position={[0, 20, 1]}
-        segments={100}
-        fade={100}
-      />
     </>
   );
 }
