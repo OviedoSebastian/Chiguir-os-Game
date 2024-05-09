c91b867669566640ded6facb037908c885eb1612
Andres Zapata <mosquera.wilson@correounivalle.edu.co>
2024-05-09 02:53:23 -0500

    Update login and add Ardilla character

--- src/pages/level1/Level1.jsx
+++ src/pages/level1/Level1.jsx
@@ -11,8 +11,10 @@ import AvatarCientific from "./characters/enemies/AvatarCientific";
 import AvatarEngineer from "./characters/avatar/AvatarEngineer";
 import useMovements from "../../utils/key-movements";
 import CharacterHud from "../hud/CharacterHud"
-import Pocion from "./collectables/Pocion";
-import Radio from "./collectables/Radio";
+import Pocion from "./collectibles/Pocion";
+import Radio from "./collectibles/Radio";
+import Ardilla from "./characters/avatar/Ardilla";
+import Buttons from "./View/Buttons";
 
 export default function Level1() {
 
@@ -46,16 +48,21 @@ export default function Level1() {
 
     useEffect(() => {
         if (radio === 5 && potion === 5) {
-            setJumpVel(6);
+            setJumpVel(10);
         }
     }, [radio, potion]);
 
     return (
+        <>
+        
         <KeyboardControls map={map} >
+            <Buttons/>
             <Canvas
                 camera={{
                     position: [0, 1, 0]
                 }}
+
+                shadows={true}
             >
                 <Suspense fallback={null}>
                     <Lights />
@@ -66,9 +73,11 @@ export default function Level1() {
                         <Radio position={[-2.5, -1.3, -4]}/>
                         <AvatarCientific position={[0, 0.42, 0]}/>
                         <AvatarEngineer />
+                        <Ardilla position={[44,5.9,-13.3]} />
                     </Physics>
                 </Suspense>
                 {/* <WelcomeText /> */}
+                
                 <Controls onJump={handleJump} onKeyA={handleLaptop} onKeyD={handlePotion}/>
             </Canvas>
             <Loader >
@@ -80,6 +89,8 @@ export default function Level1() {
                 laptop={radio}
                 potion={potion}
                 arma={arma}            />
+            
         </KeyboardControls>
+        </>
     )
 } 
