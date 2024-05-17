fdd860cafeac2dc1ef84ef78aa972e7de10c7a8e
SebastianOviedo <juan.sebastian.oviedo@correounivalle.edu.co>
2024-05-17 14:41:42 -0500

    add envairoment variables

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+import { StrictMode } from "react";
 import Experience from "./Experience";
 import "./styles.css";
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(<Experience />);
+console.log(process.env.REACT_APP_API_KEY);
+
+root.render(
+    <StrictMode>
+        <Experience />
+    </StrictMode>
+);
