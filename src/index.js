df9b92835099dd2bd0bbc0fbabe5396a7a86b267
SebastianOviedo <juan.sebastian.oviedo@correounivalle.edu.co>
2024-05-17 14:42:52 -0500

    add env variables

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
 
-console.log(process.env.REACT_APP_API_KEY);
-
 root.render(
     <StrictMode>
         <Experience />
