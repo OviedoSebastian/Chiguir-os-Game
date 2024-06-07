6f1eef1e4121eb09c6072d54a89539ec7d73bb58
SebastianOviedo <juan.sebastian.oviedo@correounivalle.edu.co>
2024-06-07 11:02:22 -0500

    Add speed Menox Se añade el recolectable del speed menox al mapa y se modifica su archivo glb

--- src/pages/level3/colectibles/SpeedMenox.jsx
+++ src/pages/level3/colectibles/SpeedMenox.jsx
@@ -3,36 +3,38 @@ import { useFrame } from "@react-three/fiber";
 import { useGLTF } from "@react-three/drei";
 import { RigidBody } from "@react-three/rapier";
 
-export default function SpeedMenox({
-  catchSpeed,
-  takeSpeedMenox,
-}) {
-  const { nodes, materials } = useGLTF(
-    "/assets/models/colectables/speedMenox.glb"
-  );
-  const [position, setPosition] = useState([5, 3, 5]);
+export default function SpeedMenox({ catchSpeed, }) {
+
+  const { nodes, materials } = useGLTF( "/assets/models/colectables/speedMenox_less.glb" );
+  const [position, setPosition] = useState([1.2, 3.2, 50]);
   const [visible, setVisible] = useState(true);
+  const [collisionCount, setCollisionCount] = useState(0);
   const refRigidBody = useRef();
   const radius = 0.3;
   const speed = 5;
-
-  useEffect(() => {
-    if (takeSpeedMenox) {
-      setVisible(false);
-    } else {
-      setVisible(true);
-    }
-  }, [takeSpeedMenox]);
-
   const [curaoSound] = useState(new Audio("/assets/sounds/CuraoSound.mp3"));
+  const lastCollisionTime = useRef(0); // Referencia para almacenar la marca de tiempo de la última colisión
+
 
   const onCollisionEnter = ({ manifold, target, other }) => {
-    // console.log("Collision at world position", manifold.solverContactPoint(0));
 
-    if (other.colliderObject.name == "character-capsule-collider") {
-      setVisible(false);
+    const currentTime = performance.now(); // Obtener la marca de tiempo actual
+    const timeSinceLastCollision = currentTime - lastCollisionTime.current;
+
+    if (other.colliderObject.name === "character-capsule-collider" & timeSinceLastCollision > 1000) {
+      setCollisionCount((prevCount) => prevCount + 1);
       curaoSound.play();
       catchSpeed();
+      
+      if (collisionCount === 0) {
+        console.log("Primera recoleccion");
+        // Change position after the first collision
+        setPosition([1, 8, -35]); // Set the new position as desired
+      } else if (collisionCount >= 1) {
+        // Hide after the second collision
+        console.log("Segunda recoleccion");
+        setVisible(false);
+      }
     }
   };
 
@@ -64,10 +66,11 @@ export default function SpeedMenox({
           name="SpeedMenox"
           position={[5, 3, 5]}
         >
-          <group dispose={null} ref={refRigidBody} >
+          <group>
             <mesh
-              geometry={nodes.Speed.geometry}
-              material={materials.SpeedMenoxColor}
+              geometry={nodes.geometry_0.geometry}
+              material={materials.Material_0}
+              position={[-0.002, 0.56, 0.011]}
             />
           </group>
         </RigidBody>
@@ -76,4 +79,4 @@ export default function SpeedMenox({
   );
 }
 
-useGLTF.preload("/assets/models/colectables/speedMenox.glb");
+useGLTF.preload("/assets/models/colectables/speedMenox_less.glb");
